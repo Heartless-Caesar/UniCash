e45e2450dd3696696c43890e98f36f4a2a1ab2d3
caesar <joaovictorcsatu@gmail.com>
2022-09-08 09:40:13 -0300

    Fixed tabbing in redeem page

--- App/Paginas/Resgate.js
+++ App/Paginas/Resgate.js
@@ -1,27 +1,46 @@
 import { View, Text, StyleSheet, Pressable } from "react-native";
 import React, { useState } from "react";
 import Lojas from "../components/lojas";
-
+import Cupons from "../components/cupons";
 const Resgate = ({ navigation }) => {
   const [shops, setShops] = useState(true);
   const [cupons, setCupons] = useState(false);
 
+  const tabToggleShops = () => {
+    setShops(true);
+    setCupons(false);
+    styles.button1 = buttonTrue;
+    styles.buttonText1 = textTrue;
+    styles.button2 = buttonFalse2;
+    styles.buttonText2 = textFalse;
+  };
+
+  const tabToggleCupons = () => {
+    setCupons(true);
+    setShops(false);
+    styles.button2 = buttonTrue2;
+    styles.buttonText2 = textTrue;
+    styles.button1 = buttonFalse;
+    styles.buttonText1 = textFalse;
+  };
+
   return (
     <View style={styles.listContainer}>
       <View style={styles.pressArea}>
-        <Pressable>
+        <Pressable onPress={() => tabToggleShops()}>
           <View style={styles.button1}>
             <Text style={styles.buttonText1}>Estabelecimentos</Text>
           </View>
         </Pressable>
-        <Pressable onPress={() => navigation.navigate("Cupons")}>
+        <Pressable onPress={() => tabToggleCupons()}>
           <View style={styles.button2}>
             <Text style={styles.buttonText2}>Cupons ativos</Text>
           </View>
         </Pressable>
       </View>
       <View style={styles.listContainer}>
-        <Lojas />
+        {shops && <Lojas />}
+        {cupons && <Cupons />}
       </View>
     </View>
   );
@@ -38,6 +57,10 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     textAlign: "center",
   },
+  buttonText1: {
+    color: "#000000",
+    textAlign: "center",
+  },
   button1: {
     borderTopLeftRadius: 5,
     backgroundColor: "#f5f5f5",
@@ -45,20 +68,16 @@ const styles = StyleSheet.create({
     padding: 15,
     width: 170,
   },
+  buttonText2: {
+    color: "#f5f5f5",
+    textAlign: "center",
+  },
   button2: {
     borderTopLeftRadius: 5,
     backgroundColor: "#800020",
     borderColor: "#cccccc",
     padding: 15,
     width: 170,
-  },
-  buttonText1: {
-    color: "#000000",
-    textAlign: "center",
-  },
-  buttonText2: {
-    color: "#f5f5f5",
-    textAlign: "center",
   },
   pressArea: {
     borderBottomColor: "#cccccc",
@@ -68,5 +87,47 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
 });
+
+const buttonTrue = {
+  borderTopLeftRadius: 5,
+  backgroundColor: "#f5f5f5",
+  borderColor: "#cccccc",
+  padding: 15,
+  width: 170,
+};
+
+const buttonTrue2 = {
+  borderTopRightRadius: 5,
+  backgroundColor: "#f5f5f5",
+  borderColor: "#cccccc",
+  padding: 15,
+  width: 170,
+};
+
+const buttonFalse = {
+  borderTopLeftRadius: 5,
+  backgroundColor: "#800020",
+  borderColor: "#cccccc",
+  padding: 15,
+  width: 170,
+};
+
+const buttonFalse2 = {
+  borderTopRightRadius: 5,
+  backgroundColor: "#800020",
+  borderColor: "#cccccc",
+  padding: 15,
+  width: 170,
+};
+
+const textTrue = {
+  color: "#000000",
+  textAlign: "center",
+};
+
+const textFalse = {
+  color: "#f5f5f5",
+  textAlign: "center",
+};
 
 export default Resgate;
