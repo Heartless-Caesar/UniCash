d7649dd18239c805139624a1b0715a5d8b33c7d4
caesar <joaovictorcsatu@gmail.com>
2022-10-01 01:08:39 -0300

    update

--- App/components/cupon_modal.jsx
+++ App/components/cupon_modal.jsx
@@ -1,8 +1,17 @@
-import { View, Text, StyleSheet, Pressable, Image, Modal } from "react-native";
 import React from "react";
+import { View, Text, StyleSheet, Pressable, Image, Modal } from "react-native";
+import cuponModalData from "../assets/data/cuponModal.json";
 
 const CuponModal = () => {
-  return <View>CuponModal</View>;
+  return (
+    <View>
+      <View style={styles.col1}>
+        <Image />
+      </View>
+      <View style={styles.col2}></View>
+      <View style={styles.col3}></View>
+    </View>
+  );
 };
 
 export default CuponModal;
