bbcf690787ec3444f18741f96193a29ac56a307d
cesa <joaovictorcsatu@gmail.com>
2022-10-16 00:34:12 -0300

    Update

--- App/App.js
+++ App/App.js
@@ -1,39 +1,41 @@
-import { React } from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import Home from "./Paginas/Home.js";
-import Details from "./Paginas/Details.js";
-import Cupons from "./components/cupons.jsx";
-import Lojas from "./components/lojas.jsx";
+import { React } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import Home from './Paginas/Home.js'
+import Details from './Paginas/Details.js'
+import Cupons from './components/cupons.jsx'
+import Lojas from './components/lojas.jsx'
+import NavigationTab from './components/navigationTab.jsx'
 
 export default function App() {
-  const Stack = createStackNavigator();
+    const Stack = createStackNavigator()
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen
-          name="Shops"
-          component={Lojas}
-          options={{ headerTitle: "" }}
-        />
-        <Stack.Screen
-          name="Details"
-          component={Details}
-          options={{ headerTitle: "" }}
-        />
-        <Stack.Screen
-          name="Cupons"
-          component={Cupons}
-          options={{ headerTitle: "" }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName="Home"
+                screenOptions={{
+                    headerShown: false,
+                }}
+            >
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen
+                    name="Shops"
+                    component={Lojas}
+                    options={{ headerTitle: '' }}
+                />
+                <Stack.Screen
+                    name="Details"
+                    component={Details}
+                    options={{ headerTitle: '' }}
+                />
+                <Stack.Screen
+                    name="Cupons"
+                    component={Cupons}
+                    options={{ headerTitle: '' }}
+                />
+            </Stack.Navigator>
+            <NavigationTab />
+        </NavigationContainer>
+    )
 }
